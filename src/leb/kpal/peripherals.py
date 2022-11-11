c15d12da2140eafa2ac57a22e3b6e1dc381f6fe5
Kyle Douglass <kyle.douglass@epfl.ch>
2022-11-11 15:15:00 +0100

    Add Attributes type to Peripheral protocol

--- src/leb/kpal/peripherals.py
+++ src/leb/kpal/peripherals.py
@@ -2,49 +2,66 @@
 
 from asyncio import StreamReader, StreamWriter
 from dataclasses import InitVar, dataclass, field
-import inspect
-from typing import Any
+from enum import Enum, auto
+from typing import Callable, Protocol, TypeAlias
 
 import serial_asyncio
 
 
-def get_attributes(peripheral: "Peripheral") -> list[Any]:
-    """Returns a list of all of a peripheral's attributes."""
-    built_in_attributes = dir(type("dummy", (object,), {}))
-    return [item for item in inspect.getmembers(peripheral) if item[0] not in built_in_attributes]
+class PeripheralState(Enum):
+    """Peripherals must be in exactly one of these states at any given time."""
+
+    PREINIT = auto()
+    INIT = auto()
+    RUNNING = auto()
+    SHUTDOWN = auto()
+    POSTSHUTDOWN = auto()
+    ERROR = auto()
 
 
-class Peripheral:
+ValueTypes: TypeAlias = bytes | float | int | str
+
+Value: TypeAlias = ValueTypes | Callable[..., ValueTypes]
+
+Attributes: TypeAlias = dict[tuple[str] | tuple[str, PeripheralState], Value]
+
+
+class Peripheral(Protocol):
     """The interface to a hardware device."""
+
+    attributes: Attributes
+
+    @property
+    def state(self) -> PeripheralState:
+        return self._state
 
 
 @dataclass
-class SerialPeripheral(Peripheral):
-    term_chars: InitVar[bytes] = "\n"
+class SerialMixin:
+    """A hardware device that employs serial communication."""
+
+    term_chars: InitVar[bytes] = b"\n"
 
     _reader: StreamReader = field(init=False, repr=False)
     _writer: StreamWriter = field(init=False, repr=False)
 
-    """A hardware device that employs serial communication."""
     def __post_init__(self, term_chars: bytes) -> None:
+        super().__init__()
         self._term_chars = term_chars
 
     @classmethod
-    async def create(cls, url: str, baudrate:int = 115200):
-        """Creates a new serial peripheral and opens a serial connection to the device.
-        
-        """
-        self = SerialPeripheral()
+    async def create(cls, url: str, baudrate: int = 115200, term_chars: bytes = b"\n"):
+        """Creates a new serial peripheral and opens a serial connection to the device."""
+        self = cls(term_chars)
         self._reader, self._writer = await serial_asyncio.open_serial_connection(
-            url,
-            baudrate=baudrate
+            url=url, baudrate=baudrate
         )
         return self
 
-    async def rx(self) -> None:
+    async def recv(self) -> None:
         """Receives a message from the device."""
-        _ = self._reader.readuntil(self._term_chars)
+        _ = await self._reader.readuntil(self._term_chars)
 
-    async def tx(self, msg: bytes) -> None:
-        """Transmits a message to the device."""
+    async def send(self, msg: bytes) -> None:
+        """Sends a message to the device."""
         self._writer.send(msg)
