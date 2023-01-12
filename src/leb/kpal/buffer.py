3b6874ee86cb11b62ce9606cbaac9eb0b2c7bc32
Kyle Douglass <kyle.douglass@epfl.ch>
2023-01-12 16:08:06 +0100

    Fix indexing errors in BufferedArray

--- src/leb/kpal/buffer.py
+++ src/leb/kpal/buffer.py
@@ -88,18 +88,27 @@ class BufferedArray(np.ndarray):
                     "Shared memory buffer %s was already closed and unlinked", self._shm.name
                 )
 
-    def put(self, data: npt.ArrayLike):
-        """Puts an existing ndarray into the buffer."""
-        arr = self._validate(np.asanyarray(data))
+    def put(self, data: npt.NDArray):
+        """Puts an existing ndarray into the buffer at the next available location."""
+        self._validate(data)
 
         # If self.size = 8, self._write_idx = 5, and arr.size = 4, then current_write_idx is -3 and
         # next_write_idx is 1.
         current_write_idx = self._write_idx
-        next_write_idx = (self._write_idx + arr.size) // self.size
-        if next_write_idx > 0:
-            current_write_idx = self._write_idx - self.size  # Should be negative
+        quotient, mod = divmod(self._write_idx + data.size, self.size)
+        if quotient > 0:
+            # Array will be inserted partly at the end and partly at the beginning of the buffer
+            current_write_idx = self._write_idx - self.size
+            next_write_idx = mod
 
-        self[current_write_idx : (next_write_idx - 1)] = np.ravel(arr)
+            assert current_write_idx < 0
+        else:
+            next_write_idx = self._write_idx + data.size
+
+        # Integer ranges won't work when wrapping around the end of the array, so use an array of
+        # indexes instead
+        indexes = list(range(current_write_idx, next_write_idx))
+        self[indexes] = np.ravel(data)
 
         self._write_idx = next_write_idx
 
@@ -110,3 +119,31 @@ class BufferedArray(np.ndarray):
 
         if arr.dtype != self.dtype:
             raise ValueError("dtypes of input array and BufferedArrays do not match")
+
+
+if __name__ == "__main__":
+    capacity = 16  # bytes
+    dtype = np.int16  # 2 bytes / integer
+
+    arr = BufferedArray(capacity=capacity, create=True, dtype=dtype)
+    
+    item1 = np.array([1, 1, 1, 1, 1], dtype)
+    item2 = np.array([2, 2, 2, 2], dtype)
+    item3 = np.array([3], dtype)
+
+    arr.put(item1)
+    assert np.all(arr[0:5] == 1)
+    assert np.all(arr[5:] == 0)
+
+    arr.put(item2)
+    assert arr[0] == 2
+    assert np.all(arr[1:5] == 1)
+    assert np.all(arr[5:] == 2)
+
+    arr.put(item3)
+    assert arr[0] == 2
+    assert arr[1] == 3
+    assert np.all(arr[2:5] == 1)
+    assert np.all(arr[5:] == 2)
+
+    arr.close()
